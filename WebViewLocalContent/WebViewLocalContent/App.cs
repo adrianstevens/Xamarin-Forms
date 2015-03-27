21631f15dba7cd43c197f7734fc83345a89ffc9e
Adrian Stevens <adrian.stevens@gmail.com>
2015-03-26 17:15:41 -0700

    Loading Html content from a PCL into a web view

---
+++ WebViewLocalContent/WebViewLocalContent/App.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace WebViewLocalContent
+{
+	public class App : Application
+	{
+		public App ()
+		{
+			// The root page of your application
+			MainPage = new MyMainPage ();
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
---
+++ WebViewLocalContent/WebViewLocalContent/MyMainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using System.IO;
+using System.Reflection;
+
+namespace WebViewLocalContent
+{
+	public partial class MyMainPage : ContentPage
+	{
+		public string htmlText { get; set; }
+		public HtmlWebViewSource htmlSource { get; set; }
+
+		public MyMainPage ()
+		{
+			BindingContext = this;
+			//
+
+			LoadHTML ();
+
+			htmlSource = new HtmlWebViewSource ();
+
+			InitializeComponent ();
+
+			//wire it up without binding
+			htmlSource.Html = htmlText;
+			AboutWebView.Source = htmlSource;
+		}
+
+		void LoadHTML ()
+		{
+			var assembly = typeof(MyMainPage).GetTypeInfo().Assembly;
+
+			Stream stream = assembly.GetManifestResourceStream("WebViewLocalContent.About.html");
+
+			using (var reader = new System.IO.StreamReader (stream)) 
+			{
+				htmlText = reader.ReadToEnd ();
+			}
+		}
+	}
+}
+
