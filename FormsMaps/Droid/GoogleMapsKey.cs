e8d40e4cd148d8b014972fb750da63c1902c40e5
Adrian Stevens <adrian.stevens@gmail.com>
2015-03-27 13:53:17 -0700

    Simple Xamarin Forms Maps XAML example

---
+++ FormsMaps/Droid/GoogleMapsKey.cs
@@ -0,0 +1,9 @@
+﻿using Android.App;
+
+// Put your Google Maps V2 API Key here.
+// See https://developers.google.com/maps/documentation/android/start#obtaining_an_api_key
+#if RELEASE
+[assembly: MetaDataAttribute("com.google.android.maps.v2.API_KEY", Value = "release_key_goes_here")]
+#else
+[assembly: MetaDataAttribute("com.google.android.maps.v2.API_KEY", Value = "AIzaSyDYGCJcdfXCoGcbjKl5KdmGXjXAOp4G5H0")]
+#endif
---
+++ FormsMaps/FormsMaps/MyMapPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using System.Diagnostics;
+using Xamarin.Forms.Maps;
+
+namespace FormsMaps
+{
+	public partial class MyMapPage : ContentPage
+	{
+		public MyMapPage ()
+		{
+			InitializeComponent ();
+
+			var posVancouver = new Xamarin.Forms.Maps.Position (49.25, -123.13);
+
+			var myPin = new Xamarin.Forms.Maps.Pin () {
+				Label = "Vancouver, BC",
+				Position = posVancouver,
+				Type = PinType.SearchResult//no effect in XM 1.4
+			};
+
+			MyMap.Pins.Add (myPin);
+
+			myPin.Clicked += (object sender, EventArgs e) => {
+
+				//not currently working - Xam Forms bug?
+				var pin = sender as Xamarin.Forms.Maps.Pin;
+
+				if(pin != null)
+				{
+					DisplayAlert ("Tapped!", "Pin was tapped.", "OK");
+					Debug.WriteLine("Pin {0} Clicked!!", pin.Label);
+				}
+			};
+
+			//move to location
+			var mapSpan = MapSpan.FromCenterAndRadius (posVancouver, Distance.FromKilometers (2.5));
+
+			MyMap.MoveToRegion(mapSpan);
+
+
+
+		
+		}
+	}
+}
+
