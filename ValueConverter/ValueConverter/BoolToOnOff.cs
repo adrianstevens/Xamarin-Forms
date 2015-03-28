c1bf4587e841a607c9a0f6e42d5cf523ac0acb15
Adrian Stevens <adrian.stevens@gmail.com>
2015-03-27 17:26:30 -0700

    Simple Xamarin Forms Value Converter example along with a handful of designer bindings

---
+++ ValueConverter/ValueConverter/BoolToOnOff.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace ValueConverter
+{
+	public class BoolToOnOff : IValueConverter
+	{
+		#region IValueConverter implementation
+
+		public object Convert (object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			
+			bool useCapLetters = false;
+
+			if(parameter != null)
+				useCapLetters = (bool)parameter;
+
+			var isOn = (bool)value;
+
+			var returnString = isOn ? "On" : "Off";
+
+			if (useCapLetters == true)
+				returnString = returnString.ToUpper ();
+
+			return returnString;
+		}
+
+		public object ConvertBack (object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			throw new NotImplementedException ();
+		}
+
+		#endregion
+	}
+}
+
