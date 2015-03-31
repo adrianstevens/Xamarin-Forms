e8bc1905b26b194f5a4e83b8ed743ce9f45855dd
Adrian Stevens <adrian.stevens@gmail.com>
2015-03-31 13:06:46 -0700

    Start of a simple Xamarin Forms localization strategy with a resx file stored in a separate PCL

---
+++ Localization/AppRes/ResMan.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AppRes
+{
+	public static class ResMan
+	{
+		public static string GetString(string id)
+		{
+			return Strings.ResourceManager.GetString (id);
+		}
+	}
+}
+
---
+++ Localization/Localization/Localization.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Localization
+{
+	public class App : Application
+	{
+		public App ()
+		{
+
+
+			// The root page of your application
+			MainPage = new ContentPage {
+				Content = new StackLayout {
+					VerticalOptions = LayoutOptions.Center,
+					Children = 
+					{
+						new Label 
+						{
+							XAlign = TextAlignment.Center,
+							Text = AppRes.ResMan.GetString("Title"), //defined in Strings.resx
+						}
+					}
+				}
+			};
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
