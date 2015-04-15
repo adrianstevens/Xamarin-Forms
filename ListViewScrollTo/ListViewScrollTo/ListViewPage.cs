52eebaea42cb3280f9a28c7d40d143c64e39b7f3
Adrian Stevens <adrian.stevens@gmail.com>
2015-04-15 11:50:43 -0700

    Added a sample using the ListView's ScrollTo method (tested on iOS & Android) - partially based on sample code I was sent - unfortunately I don't have the original source - if it's yours - thanks :)

---
+++ ListViewScrollTo/ListViewScrollTo/ListViewPage.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+using Xamarin.Forms;
+using System.Collections.ObjectModel;
+
+namespace ListViewScrollTo
+{
+	public class ListViewPage : ContentPage
+	{
+		ListView myList;
+		ObservableCollection<Person> people = PersonFactory.GetPeople();
+
+		public ListViewPage ()
+		{
+			var myGrid = new Grid ();
+
+			//list 
+			myGrid.RowDefinitions.Add (new RowDefinition () { Height = new GridLength (1, GridUnitType.Auto) });
+
+			//button
+			myGrid.RowDefinitions.Add (new RowDefinition () { Height = new GridLength (1, GridUnitType.Star) });
+
+			//add list
+			myList = CreateList ();
+			myList.ItemsSource = people;
+			myList.ItemTapped += MyList_ItemTapped;
+			Grid.SetRow (myList, 1);
+
+			myGrid.Children.Add (myList);
+
+			//add button
+			var myButton = new Button () {
+				HorizontalOptions = LayoutOptions.CenterAndExpand,
+				VerticalOptions = LayoutOptions.Center,
+				HeightRequest = 40,
+				Text = "Scroll to Random Item",
+			};
+			myButton.Clicked += MyButton_Clicked;
+
+
+			Grid.SetRow (myButton, 0);
+			myGrid.Children.Add (myButton);
+
+
+			Content = myGrid;
+
+		}
+
+		//use item slected if you want an event to fire when items are programatically selected
+		void MyList_ItemTapped (object sender, ItemTappedEventArgs e)
+		{
+			DisplayAlert ((e.Item as Person).Name, "ListView Item Tapped", "ok");
+		}
+
+		void MyButton_Clicked (object sender, EventArgs e)
+		{
+			int index = new Random ().Next (people.Count);
+
+			myList.ScrollTo (people [index], ScrollToPosition.Center, true);
+			myList.SelectedItem = people [index];
+		}
+
+		ListView CreateList ()
+		{
+			var listView = new ListView
+			{
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				VerticalOptions = LayoutOptions.FillAndExpand,
+				ItemTemplate = new DataTemplate(() =>
+					{
+						var nameLabel = new Label();
+						var birthdayLabel = new Label();
+						var boxView = new BoxView();
+
+						var stack = new StackLayout
+						{
+							
+							Padding = new Thickness(5),
+							Orientation = StackOrientation.Horizontal,
+							BackgroundColor = Color.Black,
+							Children =
+							{
+								boxView,
+								new StackLayout
+								{
+									VerticalOptions = LayoutOptions.Center,
+									Spacing = 0,
+									Children =
+									{
+										nameLabel,
+										birthdayLabel
+									}
+								}
+							}
+						};
+
+						nameLabel.SetBinding(Label.TextProperty, "Name");
+						birthdayLabel.SetBinding(Label.TextProperty, new Binding("Birthday", BindingMode.OneWay, null, null, "Born {0:d}"));
+						boxView.SetBinding(BoxView.ColorProperty, "FavoriteColor");
+						stack.SetBinding(BackgroundColorProperty, "BackgroundColor");
+
+						return new ViewCell
+						{
+							View = stack
+						};
+					})
+			};
+
+			return listView;
+		}
+	}
+}
+
+
---
+++ ListViewScrollTo/ListViewScrollTo/Models/Person.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Collections.ObjectModel;
+
+namespace ListViewScrollTo
+{
+	public class Person
+	{
+		public string Name { private set; get; }
+		public DateTime Birthday { private set; get; }
+		public Color FavoriteColor { private set; get; }
+
+		public Person(string name, DateTime birthday, Color favColor)
+		{
+			this.Name = name;
+			this.Birthday = birthday;
+			this.FavoriteColor = favColor;
+		}
+	}
+
+	public static class PersonFactory
+	{
+		public static ObservableCollection<Person> GetPeople ()
+		{
+			return new ObservableCollection<Person> {
+				new Person ("Abby", new DateTime (1975, 1, 15), Color.Aqua),
+				new Person ("Bob", new DateTime (1976, 2, 20), Color.Black),
+				new Person ("Kath", new DateTime (1977, 3, 10), Color.Blue),
+				new Person ("David", new DateTime (1978, 4, 25), Color.Fuchsia),
+				new Person ("Tori", new DateTime (1975, 1, 15), Color.Gray),
+				new Person ("Nathan", new DateTime (1976, 1, 15), Color.Green),
+				new Person ("Jorge", new DateTime (1977, 1, 15), Color.Lime),
+				new Person ("Lisa", new DateTime (1975, 1, 15), Color.Maroon),
+				new Person ("Scott", new DateTime (1976, 1, 15), Color.Navy),
+				new Person ("Sara", new DateTime (1977, 1, 15), Color.Olive),
+				new Person ("John", new DateTime (1975, 1, 15), Color.Pink),
+				new Person ("Emily", new DateTime (1976, 1, 15), Color.Purple),
+				new Person ("Cindy", new DateTime (1977, 1, 15), Color.Red),
+				new Person ("Donny", new DateTime (1978, 1, 15), Color.Silver),
+				new Person ("Marco", new DateTime (1977, 1, 15), Color.Teal),
+				new Person ("Cesar", new DateTime (1976, 1, 15), Color.Transparent),
+				new Person ("Adrian", new DateTime (1975, 1, 15), Color.White),
+				new Person ("Matt", new DateTime (1974, 1, 15), Color.Yellow),
+
+				new Person ("Chris", new DateTime (1975, 1, 15), Color.Aqua),
+				new Person ("Rene", new DateTime (1976, 2, 20), Color.Black),
+				new Person ("Mark", new DateTime (1977, 3, 10), Color.Blue),
+				new Person ("Mark", new DateTime (1978, 4, 25), Color.Fuchsia),
+				new Person ("Rob", new DateTime (1975, 1, 15), Color.Gray),
+				new Person ("Bryan", new DateTime (1976, 1, 15), Color.Green),
+				new Person ("Karina", new DateTime (1977, 1, 15), Color.Lime),
+				new Person ("Judy", new DateTime (1975, 1, 15), Color.Maroon),
+				new Person ("Helen", new DateTime (1976, 1, 15), Color.Navy),
+				new Person ("Zulma", new DateTime (1977, 1, 15), Color.Olive),
+				new Person ("Kym", new DateTime (1975, 1, 15), Color.Pink),
+				new Person ("Glenn", new DateTime (1976, 1, 15), Color.Purple),
+				new Person ("Adam", new DateTime (1977, 1, 15), Color.Red),
+				new Person ("Amy", new DateTime (1978, 1, 15), Color.Silver),
+				new Person ("Anuj", new DateTime (1977, 1, 15), Color.Teal),
+				new Person ("Betai", new DateTime (1976, 1, 15), Color.Transparent),
+				new Person ("Charles", new DateTime (1975, 1, 15), Color.White),
+				new Person ("Craig", new DateTime (1974, 1, 15), Color.Yellow),
+			};
+		}
+	}
+}
+
