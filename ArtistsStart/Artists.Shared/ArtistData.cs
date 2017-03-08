7c2b1c9bf57b35a2fa20a7f5f36b3de91f18d6e6
Adrian Stevens <adrian.stevens@gmail.com>
2017-03-08 13:09:54 -0800

    removed "Artists" listview projects

--- ArtistsStart/Artists.Shared/ArtistData.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Artists.Shared
-{
-    public static class ArtistData
-    {
-        public static List<string> Artists = new List<string>() { "artist1", "artist2", "artist3" };
-    }
-}
--- ArtistsXF/Artists.Shared/ArtistData.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Artists.Shared
-{
-    public static class ArtistData
-    {
-        public static List<string> Artists = new List<string>() { "Aretha Franklin", "Madonna", "Emimen", "Mini pops" };
-    }
-}
