c96e43682d9d33b4d442be2384dce96cc86e717f
Adrian <adrian.stevens@gmail.com>
2017-09-08 17:26:11 -0700

    Added an OxyPlot Xamarin.Forms sample (mostly works)

---
+++ OxyPlotSample/OxyPlotSample/OxyPlotSample/MainPage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace OxyPlotSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public PlotModel Model { get; set; }
+
+        public MainPage()
+        {
+            this.Model = CreateNormalDistributionModel();
+
+            InitializeComponent();
+
+            this.BindingContext = this;
+        }
+
+        /// <summary>
+        /// Creates a model showing normal distributions.
+        /// </summary>
+        /// <returns>A PlotModel.</returns>
+
+        static PlotModel CreateNormalDistributionModel()
+        {
+            // http://en.wikipedia.org/wiki/Normal_distribution
+            var plot = new PlotModel
+            {
+                Title = "Normal distribution",
+                Subtitle = "Probability density function"
+            };
+
+            plot.Axes.Add(new LinearAxis
+            {
+                Position = AxisPosition.Left,
+                Minimum = -0.05,
+                Maximum = 1.05,
+                MajorStep = 0.2,
+                MinorStep = 0.05,
+                TickStyle = TickStyle.Inside
+            });
+
+            plot.Axes.Add(new LinearAxis
+            {
+                Position = AxisPosition.Bottom,
+                Minimum = -5.25,
+                Maximum = 5.25,
+                MajorStep = 1,
+                MinorStep = 0.25,
+                TickStyle = TickStyle.Inside
+            });
+
+            plot.Series.Add(CreateNormalDistributionSeries(-5, 5, 0, 0.2));
+            plot.Series.Add(CreateNormalDistributionSeries(-5, 5, 0, 1));
+            plot.Series.Add(CreateNormalDistributionSeries(-5, 5, 0, 5));
+            plot.Series.Add(CreateNormalDistributionSeries(-5, 5, -2, 0.5));
+
+            return plot;
+
+        }
+
+        static LineSeries CreateNormalDistributionSeries(double x0, double x1, double mean, double variance, int n = 1000)
+        {
+            var ls = new LineSeries
+            {
+                Title = string.Format("μ={0}, σ²={1}", mean, variance)
+            };
+
+            for (int i = 0; i < n; i++)
+            {
+                double x = x0 + ((x1 - x0) * i / (n - 1));
+                double f = 1.0 / Math.Sqrt(2 * Math.PI * variance) * Math.Exp(-(x - mean) * (x - mean) / 2 / variance);
+                ls.Points.Add(new DataPoint(x, f));
+            }
+
+            return ls;
+        }
+    }
+}
