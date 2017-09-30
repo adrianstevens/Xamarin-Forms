cf12af0bc2bbc4fbf5c6d7609e07037efae0dca1
Adrian Stevens <adrian.stevens@gmail.com>
2017-09-30 15:53:37 -0700

    DrumPad - added macOS head project and updated Xamarin.Forms nuget packages to 2.4 (except UWP proj)

---
+++ DrumPad/DrumPad.macOS/AppDelegate.cs
@@ -0,0 +1,48 @@
+﻿using AppKit;
+using Foundation;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+using DrumPad;
+
+namespace DrumPad.macOS
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : FormsApplicationDelegate
+    {
+        NSWindow window;
+
+        public override NSWindow MainWindow
+        {
+            get
+            {
+                return window;
+            }
+        }
+
+        public AppDelegate()
+        {
+            var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
+
+            var rect = new CoreGraphics.CGRect(100, 100, 1024, 768);
+            window = new NSWindow(rect, style, NSBackingStore.Buffered, false);
+            window.Title = "DrumPad";
+            window.TitleVisibility = NSWindowTitleVisibility.Hidden;
+
+        }
+
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            Forms.Init();
+			DrumPad.App.CreateAudioPlayer = () => new DrumPad.macOS.SimpleAudioPlayer();
+
+			LoadApplication(new App());
+            base.DidFinishLaunching(notification);
+        }
+
+        public override void WillTerminate(NSNotification notification)
+        {
+            // Insert code here to tear down your application
+        }
+    }
+}
---
+++ DrumPad/DrumPad.macOS/SimpleAudioPlayer.macOS.cs
@@ -0,0 +1,54 @@
+using AVFoundation;
+using Foundation;
+using SimpleAudio;
+using System;
+using System.IO;
+
+namespace DrumPad.macOS
+{
+    class SimpleAudioPlayer : ISimpleAudioPlayer
+    {
+        AVAudioPlayer player;
+
+        public bool Load (Stream audioStream)
+        {
+            var data = NSData.FromStream(audioStream);
+
+            Stop();
+            player?.Dispose();
+
+            player = AVAudioPlayer.FromData(data);
+
+            return (player == null) ? false : true;
+        }
+
+        public void Play()
+        {
+            if (player == null)
+                return;
+
+            if(player.Playing)
+                player.CurrentTime = 0;
+            else
+                player?.Play();
+        }
+
+        public void Pause()
+        {
+            player?.Pause();
+        }
+
+        public void Stop()
+        {
+            player?.Stop();
+        }
+
+        public void SetVolume(double volume)
+        {
+            volume = Math.Max(0, volume);
+            volume = Math.Min(1, volume);
+
+            player.SetVolume((float)volume, (float)volume);
+        }
+    }
+}
